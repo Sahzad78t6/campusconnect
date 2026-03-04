eb45cb5fdfbc1c69b960011f126f120a494d987c
Sahzad78t6 <vu.241fa04d82@gmail.com>
2026-03-04 22:18:01 +0530

    update login

--- config/passport.js
+++ config/passport.js
@@ -4,11 +4,11 @@ const User = require("../models/User");
 
 passport.use(
   new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback"
-    },
+{
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "https://campusconnect-bcqq.onrender.com/api/auth/google/callback"
+},
     async (accessToken, refreshToken, profile, done) => {
       try {
 
