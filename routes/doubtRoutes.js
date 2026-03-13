25f0fdfeeb92e4503110330afb703b8cbd7a7bde
Sahzad78t6 <vu.241fa04d82@gmail.com>
2026-03-14 05:25:17 +0530

    added gitignore

--- routes/doubtRoutes.js
+++ routes/doubtRoutes.js
@@ -135,23 +135,5 @@ router.put("/:doubtId/answer/:answerId/pin", auth, admin, async (req, res) => {
   }
 });
 
-const axios = require("axios");
 
-async function askGemini(question) {
-
-  const response = await axios.post(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
-    {
-      contents: [
-        {
-          parts: [{ text: question }]
-        }
-      ]
-    }
-  );
-
-  return response.data.candidates[0].content.parts[0].text;
-}
-
-module.exports = askGemini;
 module.exports = router;
